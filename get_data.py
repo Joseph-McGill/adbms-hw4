5751e028a4d98356b59e6502dab9d10ff67fd790
Joseph McGill <joseph.a.mcgill@gmail.com>
2017-04-20 15:58:41 -0500

    Printed files

--- get_data.py
+++ get_data.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
 
+import re
+import os.path
+from bs4 import BeautifulSoup
+from collections import Counter
+from datetime import datetime
+from nltk.tokenize import word_tokenize
+from scipy import spatial
 from urllib.request import urlretrieve
 from urllib.request import urlopen
 from urllib.error import HTTPError
-from bs4 import BeautifulSoup
-import codecs
-import re
-import string
-import nltk
-from collections import Counter
-from nltk.tokenize import word_tokenize, sent_tokenize
-from scipy import spatial
-from datetime import datetime
-import os.path
-import numpy as np
 
-### GLOBAL VARIABLES ### #TODO
+
+### GLOBAL VARIABLES ###
 # Stop words
 with open('stopwords.txt', 'r') as in_file:
     STOP_WORDS = set([word.strip() for word in in_file.readlines()])
@@ -24,8 +21,8 @@ with open('stopwords.txt', 'r') as in_file:
 REGEX = re.compile('[^a-zA-Z]')        
 
 # Threshold for the similarity
-#SIMILARITY_THRESHOLD = float(input('Enter a threshold value: '))
-SIMILARITY_THRESHOLD = 0.75 #TODO
+SIMILARITY_THRESHOLD = float(input('Enter a threshold value: '))
+
 
 # Class representing a book
 class Book:
@@ -108,10 +105,10 @@ def get_book(book_num):
     regex_title = re.search(r'(.*) by\b', title)
     
     if regex_title is not None:
-        title = regex_title.group(1)#.strip()
+        title = regex_title.group(1)
         
     # get the book's text from the downloaded file
-    with codecs.open(file_name, encoding='latin-1') as book:
+    with open(file_name, 'r') as book:
         
         # get the full text
         text = book.read()
@@ -123,8 +120,11 @@ def get_book(book_num):
     # return the book object
     return Book(book_num, title, author_name, author_birth_year, pub_date, text, words)
 
+
 # Function to calculate and print the book similarities to a file
 def print_similarities(books):
+
+    # create a set of the distinct words from every book
     word_set = set() 
     for book in books:
         word_set |= set(book.words)
@@ -132,6 +132,7 @@ def print_similarities(books):
     # turn the set of all words into a sorted list
     word_list = sorted(list(word_set))
     
+    # create the weight vectors for each book
     for book in books:
         counts = Counter(book.words)
         
@@ -142,13 +143,15 @@ def print_similarities(books):
         
         book.vector = vector
     
+    # calculate and print the cosine similartiy values
     with open('similarities.txt', 'w') as out:
         for i in range(len(books)):
             for j in range(len(books)):
                 similarity = 1 - spatial.distance.cosine(books[i].vector, books[j].vector)
                 if (similarity >= SIMILARITY_THRESHOLD and
                     books[i].pub_date <= books[j].pub_date and i is not j):
-                  out.write("Book %d, Book %d, %f\n" % (i+1, j+1, similarity))
+                  out.write("%d\t%d\t%f\n" % (i+1, j+1, similarity))
+
 
 # Function to print the authors of the books to a file
 def print_authors(books):
@@ -163,18 +166,19 @@ def print_authors(books):
     with open('authors.txt', 'w') as out:
         author_id = 1 
         for author in author_set:
-            
-            out.write("%d; %s; %s\n" % (author_id, author[0], author[1]))
+            out.write("%d\t%s\t%s\n" % (author_id, author[0], author[1]))
             author_id += 1      
+
 
 # Function to print the book information to a file
 def print_books(books):
     
     with open('books.txt', 'w') as out:
         for book in books:
-            out.write("%d; %s; %s; %s\n" % 
+            out.write("%d\t%s\t%s\t%s\n" % 
             (book.book_num, book.title, book.author_name, 
-            book.pub_date.strftime("%b %d, %Y")))
+            book.pub_date.strftime("%Y-%m-%d")))
+   
     
 # Main Function
 def main():
@@ -186,10 +190,14 @@ def main():
     
     # calculate the cosine similarities between 
     # the books and print them to a file
-    #print_similarities(books)
-     
-    #print_authors(books)
+    print_similarities(books)
+    
+    # print the author information to a file 
+    print_authors(books)
+    
+    # print the book information to a file
     print_books(books)
+ 
                 
 # Run the main function  
 if __name__ == '__main__':
