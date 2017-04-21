1a865429737fb1a302257ada1463eab6e7c9aad6
Joseph McGill <joseph.a.mcgill@gmail.com>
2017-04-21 15:56:36 -0500

    Update

--- get_data.py
+++ get_data.py
@@ -126,7 +126,7 @@ def print_similarities(books):
 
     # create a set of the distinct words from every book
     word_set = set() 
-    for book in books:
+    for book in books:h
         word_set |= set(book.words)
         
     # turn the set of all words into a sorted list
